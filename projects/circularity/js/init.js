95cb85526337c2b36a314806761743856e9ecde6
morgan carr <morgan.carr@jpschools.org>
2023-11-30 19:35:08 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -77,14 +77,14 @@ var circles = [];
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if(circle.x < 0){
-                circle.x = 0;
+            else if(circle.x < 0){
+                circle.x = canvas.width;
             }
-            if(circle.y < 0){
-                circle.y = 0
+            else if(circle.y < 0){
+                circle.y = canvas.height;
             }
-            if(circle.y > canvas.height){
-                circle.y = 0
+            else if(circle.y > canvas.height){
+                circle.y = 0;
             }
 
 
